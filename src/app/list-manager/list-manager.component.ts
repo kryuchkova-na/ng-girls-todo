307d4b1c8dd327d569f19c56bb2a28082c000644
Nataliya Kryuchkova <pnokka@yandex.ru>
2019-11-11 15:27:16 +0300

    step18

--- src/app/list-manager/list-manager.component.ts
+++ src/app/list-manager/list-manager.component.ts
@@ -9,7 +9,7 @@ import {TodoListService} from '../services/todo-list.service';
       <app-input-button-unit (submit)="addItem($event)"></app-input-button-unit>
       <ul>
           <li *ngFor="let todoItem of todoList">
-              <app-todo-item [item]="todoItem"></app-todo-item>
+              <app-todo-item [item]="todoItem" (remove)="removeItem($event)"></app-todo-item>
           </li>
       </ul>
     </div>`,
@@ -26,5 +26,9 @@ export class ListManagerComponent implements OnInit {
 
   addItem(title: string) {
     this.todoListService.addItem({ title })
+  }
+
+  removeItem(item) {
+    this.todoListService.deleteItem(item);
   }
 }
