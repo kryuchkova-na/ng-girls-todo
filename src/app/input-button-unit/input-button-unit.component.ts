10489ecf6ea19386dde8904562bd60163a5913b8
Nataliya Kryuchkova <pnokka@yandex.ru>
2019-11-11 14:28:45 +0300

    step14

--- src/app/input-button-unit/input-button-unit.component.ts
+++ src/app/input-button-unit/input-button-unit.component.ts
@@ -2,15 +2,16 @@ import {Component, EventEmitter, OnInit, Output} from '@angular/core';
 
 @Component({
   selector: 'app-input-button-unit',
-  template: `<input #inputElementRef
-         [value]="title"
-         (keyup.enter)="submitValue($event.target.value)">
-  <button (click)="submitValue(inputElementRef.value)">Save</button>
+  template: `<input class="todo-input"
+                    #inputElementRef
+                    [value]="title"
+                    (keyup.enter)="submitValue($event.target.value)">
+  <button class="btn" (click)="submitValue(inputElementRef.value)">Save</button>
   `,
   styleUrls: ['./input-button-unit.component.css']
 })
 export class InputButtonUnitComponent implements OnInit {
-  title = 'Hello World!';
+  title = '';
   @Output() submit: EventEmitter<string> = new EventEmitter();
 
   constructor() {
--- src/app/list-manager/list-manager.component.ts
+++ src/app/list-manager/list-manager.component.ts
@@ -4,12 +4,14 @@ import {TodoItem} from '../interfaces/todo-item';
 @Component({
   selector: 'app-list-manager',
   template: `
+    <div class="todo-app">
       <app-input-button-unit (submit)="addItem($event)"></app-input-button-unit>
       <ul>
           <li *ngFor="let todoItem of todoList">
               <app-todo-item [item]="todoItem"></app-todo-item>
           </li>
-      </ul>`,
+      </ul>
+    </div>`,
   styleUrls: ['./list-manager.component.css']
 })
 export class ListManagerComponent implements OnInit {
